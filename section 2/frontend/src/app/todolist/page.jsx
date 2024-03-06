16ea55c79929b93f466295643734278dfb0600f6
AkankshaGandhi23 <gandhiakanksha106@gmail.com>
2024-03-06 18:24:51 +0530

    react

--- section 2/frontend/src/app/todolist/page.jsx
+++ section 2/frontend/src/app/todolist/page.jsx
@@ -13,8 +13,33 @@ const TodoList = () => {
 
 
     const addNewTask = (e) => {
-    console.log(e.key);
+    
+    if(e.code ==='Enter'){
+        console.log(e.target.value);
+        const obj ={text :e.target.value , completed :false};
+
+        setTaskList ([obj , ...taskList]);
+
+        e.target.value ='';
+    }
 }
+
+const deleteTask =(index) => {
+    console.log(index);
+    const temp =taskList;
+    temp.splice(index , 1);
+    setTaskList([...temp]);
+}
+
+const completeTask =(index) =>
+{
+    const temp =taskList;
+    temp[index].completed = true;
+    setTaskList([...temp]);
+    console.log(temp);
+}
+
+
 
 
   return (
@@ -23,7 +48,10 @@ const TodoList = () => {
         <h1 className='display-3 fw-bold text-center'>TodoList</h1>
         <div className="card shadow">
             <div className='card-header py-3'>
-                <input type='text' className=' form-control border-3 border-primary '
+
+                <input onKeyDown={addNewTask} 
+                
+                type='text' className=' form-control border-3 border-primary '
                  placeholder='Enter new task to add'/>
 
 
@@ -32,10 +60,20 @@ const TodoList = () => {
             <div className='card-body'>
                 {
                     taskList.map((task , index) => {
-                        return <div key={index} className='p-3 shadow mb-3 d-flex justify-content-between'>  
+                        return <div key={index} className='p-3 shadow mb-3 d-flex justify-content-between align-items-center'>  
+                        <input type='checkbox' checked={task.completed}/>
                         <p className='my-auto h4'>{ task.text }</p>
+                        {task.completed ? 
+                        <span className=' badge bg-success'>completed </span>
+                    : 
+                    <span className='badge bg-warning' > Pending </span>
+                    }
 
-                        <button className='btn btn-danger'> Delete </button>
+                        <button onClick={() => { deleteTask(index)} } className='btn btn-danger'> Delete </button>
+                        <button onClick={() => {completeTask(index)}} className='btn btn-primary' >
+                         
+                         {task.completed ?'Undo Task' : 'Complete Task'}
+                         </button>
                     </div>
                     } )
                 }
